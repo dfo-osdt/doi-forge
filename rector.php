6acd796262daea5b0acd5f7f2d3a6da4df4804ac
Vincent Auger <vincent.auger@dfo-mpo.gc.ca>
2026-08-31 11:32:53 -0300

    chore(deps): update

--- rector.php
+++ rector.php
@@ -3,17 +3,12 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 use RectorLaravel\Set\LaravelSetList;
-use RectorLaravel\Set\LaravelSetProvider;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/app',
         __DIR__.'/tests',
-    ])
-    ->withSkip([
-        AddOverrideAttributeToOverriddenMethodsRector::class,
     ])
     ->withPreparedSets(
         deadCode: true,
@@ -23,7 +18,6 @@ return RectorConfig::configure()
         earlyReturn: true,
     )
     ->withPhpSets()
-    ->withSetProviders(LaravelSetProvider::class)
     ->withComposerBased(laravel: true)
     ->withSets([
         LaravelSetList::LARAVEL_CODE_QUALITY,
